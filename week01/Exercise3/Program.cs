ca2fcd9bff7dd3b1d5ff282b2ad6193694732b9f
olisebinum <ebinumolise001@gmail.com>
2026-07-01 14:54:46 +0100

    feat: complete Exercise 3 with game loop and random number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        Random random = new Random();
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // Generate a random magic number between 1 and 100
+            int magicNumber = random.Next(1, 101);
+            int guess = 0;
+            int guessCount = 0;
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+            // Keep asking until the user guesses correctly
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            Console.Write("Would you like to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
